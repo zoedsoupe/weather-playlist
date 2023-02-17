c17e6894990f1ddbe2460c80f9dd3c86466387a8
Zoey de Souza Pessanha <zoey.pessanha@nubank.com.br>
2023-02-16 21:43:42 -0300

    feat: add dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,12 @@
   :description "Generates a playlist based on a city temperature"
   :url "https://weather-playlist.fly.dev"
   :license {:name "BSD 3-Clause"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [io.pedestal/pedestal.service "0.5.7"]
+                 [io.pedestal/pedestal.route "0.5.7"]
+                 [io.pedestal/pedestal.jetty "0.5.7"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.slf4j/slf4j-simple "1.7.28"]]
   :main ^:skip-aot weather-playlist.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
